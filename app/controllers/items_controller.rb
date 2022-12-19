953bd76078b18d1174b63c8d67f833482087f8f8
iizun <118422975+iizun@users.noreply.github.com>
2022-12-19 15:26:11 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:edit]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -24,23 +24,22 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-      if @item.user_id != current_user.id
-        redirect_to root_path
-      end
+    redirect_to root_path if @item.user_id != current_user.id
   end
 
   def update
-   @item = Item.find(params[:id])
-   if @item.update(item_params)
-    redirect_to item_path(params[:id])
-   else
-    render :edit
-   end
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :summary, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,:delivery_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :summary, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
+                                 :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 end
