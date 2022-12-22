c2506f87279bc5b316889ea95760b81ae8e73515
iizun <118422975+iizun@users.noreply.github.com>
2022-12-22 15:31:51 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :item_find, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order = @item.order
     return redirect_to root_path if @item.user_id == current_user.id || @order != nil
     @order_ship = OrderShip.new
@@ -11,6 +11,7 @@ class OrdersController < ApplicationController
   def create
     @order_ship = OrderShip.new(order_ship_params)
     if @order_ship.valid?
+      pay_item
       @order_ship.save
       redirect_to root_path
     else
@@ -20,8 +21,21 @@ class OrdersController < ApplicationController
 
   private
 
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
+
   def order_ship_params
-    params.require(:order_ship).permit(:post_code, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_ship).permit(:post_code, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_ship_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order_ship.rb
+++ app/models/order_ship.rb
@@ -1,12 +1,13 @@
 class OrderShip
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_number, :phone_number , :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_number, :phone_number , :user_id, :item_id, :token
 
   with_options presence: true do
     validates :city
     validates :house_number
     validates :user_id
     validates :item_id
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
   end
