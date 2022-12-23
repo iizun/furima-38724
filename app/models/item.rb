711feb264d9405208cd46149f5bc9e745ec8fa43
iizun <118422975+iizun@users.noreply.github.com>
2022-12-23 16:25:41 +0900

    エラー表示の日本語化を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,13 +12,13 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :summary, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :condition_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :delivery_day_id, numericality: { other_than: 1, message: "を入力してください" }
   validates :image, presence: true
 
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '半角数字で¥300〜9,999,999の範囲で入力してください' }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は半角数字で¥300〜9,999,999の範囲で入力してください' }
 end
